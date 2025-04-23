432d9f477d134af09ef5f65e392db04b9df4e701
Abbazs Rahman Mohamed Sadahath <abbazsr795@gmail.com>
2025-04-23 12:30:15 +1000

    feat : supabase authentication is set up. sign in and sign up works

---
+++ trai-frontend/src/app/(store)/store.tsx
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import credentialsReducer from './slices/credentials';
+
+export const store = configureStore({
+  reducer: {
+    credentials : credentialsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ trai-frontend/src/app/signin/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../(store)/store";
+import { setEmail, setPassword, setToken, clearEmail, clearPassword, clearToken } from "../(store)/slices/credentials";
+import supabase from "../(supabase)/config";
+
+export default function SignIn() {
+
+    const email = useSelector((state: RootState) => state.credentials.email);
+    const password = useSelector((state: RootState) => state.credentials.password);
+    const token = useSelector((state: RootState) => state.credentials.token);
+    const dispatch = useDispatch();
+
+    const handleSignIn = async () => {
+        let { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password
+        })
+        clearEmail();
+        clearPassword();
+    }
+
+    return(
+        <div>
+            <h1>email</h1>
+            <input type="text" placeholder="Email" onChange={(e) => dispatch(setEmail(e.target.value))} />
+            <h1>password</h1>
+            <input type="password" placeholder="Password" onChange={(e) => dispatch(setPassword(e.target.value))} />
+            <button onClick={() => handleSignIn()}>Submit</button>
+        </div>
+    )
+}
