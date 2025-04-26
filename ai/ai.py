a98478252457311fdf5665860e7024c0928f62ee
Anubhav Patra <s4099336@student.rmit.edu.au>
2025-04-26 14:39:38 +1000

    ..

--- ai/ai.py
+++ ai/ai.py
@@ -27,10 +27,11 @@ def get_client() -> genai.Client:
 
 # Function to get the Gemini response
 def get_response(image_data: bytes, client: genai.Client) -> Dict[str, str]:
+    uploaded_image = genai.upload_file(image_data, mime_type="image/png")
     response: GenerateContentResponse = client.models.generate_content(
         model='gemini-1.5-flash',
         contents=[
-            {"mime_type": "image/png", "data": image_data},
+            {"mime_type": "image/png", "data": uploaded_image},
             "Classify the object. Classify the object in this image into one of the following categories: " +
             "'perfectly reusable', 'reusable with some level of tinkering', or 'can't be used anymore'." +
             " Only give the name of the class and not any text."
@@ -74,7 +75,7 @@ async def classify_image(file: UploadFile = File(...)):
     
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",  # "main" is your filename (without .py), "app" is your FastAPI instance
+        "ai:app",  # "main" is your filename (without .py), "app" is your FastAPI instance
         host="0.0.0.0",
         port=8000,
         reload=True  # optional, for development (auto-reload on code change)
