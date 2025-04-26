6511df1701bbb78da4a048d1d1648cbb22b74543
Anubhav Patra <s4099336@student.rmit.edu.au>
2025-04-26 16:12:22 +1000

    ..

--- ai/ai.py
+++ ai/ai.py
@@ -89,6 +89,6 @@ if __name__ == "__main__":
         "ai:app",  
         host="0.0.0.0",
         port=8000,
-        reload=True  # optional, for development (auto-reload on code change)
+        reload=True 
     )
     
